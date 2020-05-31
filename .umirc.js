e76f7b6ad4db551ae03cc5d18956f5a2d013b32f
吴杰 <676899459@qq.com>
2020-05-31 14:18:22 +0800

    代码修改

--- .umirc.js
+++ .umirc.js
@@ -1,24 +1,24 @@
-import { defineConfig } from 'umi';
+import { defineConfig } from "umi";
 //关于环境变量的配置可参考这里sorrycc的评论, 目前(2020年5月31日)没照他的这个方式来, 以后如果不满足了再做调整
 //https://github.com/umijs/umi/issues/2079#issuecomment-471176545
 
 export default defineConfig({
   nodeModulesTransform: {
-    type: 'none',
+    type: "none"
   },
   ssr: {
     forceInitial: false,
     devServerRender: true,
-    mode: 'stream',
-    staticMarkup: false,
+    mode: "stream",
+    staticMarkup: false
   },
   targets: {
-    ie: 9,
+    ie: 9
   },
   autoprefixer: false,
   define: {
-    env: {
-      domain: `${process.env.domain}`,
-    },
-  },
+    "process.env": {
+      domain: `${process.env.domain}`
+    }
+  }
 });
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,27 @@
-import { PureComponent } from 'react';
-import styles from './index.less';
-import CompSrc from '@/components/CompSrc';
+import { PureComponent } from "react";
+import PropTypes from "prop-types";
+import styles from "./index.less";
+import CompSrc from "@/components/CompSrc";
 
 class Index extends PureComponent {
   static async getInitialProps(ctx) {
-    console.log('ctx', ctx);
+    console.log("ctx", ctx);
 
     return {
       data: {
-        content: 'hello world',
-      },
+        content: "hello world"
+      }
     };
   }
+
+  static propTypes = {
+    data: PropTypes.object.isRequired
+  };
 
   render() {
     const { data } = this.props;
     const { content } = data || {};
 
-    const { domain } = env;
-    console.log(domain);
     return (
       <div>
         <h1 className={styles.title}>{content}</h1>
