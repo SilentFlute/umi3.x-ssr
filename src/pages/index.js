a71dc6acf62d8f780508d0fa16070c69edf38ad0
吴杰 <676899459@qq.com>
2020-05-31 14:25:21 +0800

    删除git/package.json的一些预检测和生命周期

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,15 @@
-import { PureComponent } from "react";
-import PropTypes from "prop-types";
-import styles from "./index.less";
-import CompSrc from "@/components/CompSrc";
+import { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import styles from './index.less';
+import CompSrc from '@/components/CompSrc';
 
 class Index extends PureComponent {
   static async getInitialProps(ctx) {
-    console.log("ctx", ctx);
+    console.log('ctx', ctx);
 
     return {
       data: {
-        content: "hello world"
+        content: 'hello world'
       }
     };
   }
