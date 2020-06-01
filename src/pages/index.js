a2e480200117fa835880e6d946a0167fb3d406df
吴杰 <676899459@qq.com>
2020-06-01 10:10:04 +0800

    使用延时来模拟请求效果

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,27 @@ import PropTypes from 'prop-types';
 import styles from './index.less';
 import CompSrc from '@/components/CompSrc';
 
+const wait = time => (
+  new Promise((res, rej) => {
+    setTimeout(
+      () => {
+        res({
+          content: 'hello world'
+        });
+      },
+      time
+    );
+  })
+);
+
 class Index extends PureComponent {
   static async getInitialProps(ctx) {
     console.log('ctx', ctx);
 
+    const data = await wait(3000);
+
     return {
-      data: {
-        content: 'hello world'
-      }
+      data
     };
   }
 
