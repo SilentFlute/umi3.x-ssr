58f584f66fe29847c244f1eb6f47c72a3d5bd015
吴杰 <676899459@qq.com>
2020-06-01 14:17:13 +0800

    添加express测试打包部署

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+const domain = 'http://localhost';
+
+// Express
+app.use(async (req, res) => {
+  // 或者从 CDN 上下载到 server 端
+  // const serverPath = await downloadServerBundle('http://cdn.com/bar/umi.server.js');
+  const render = require('./dist/umi.server');
+  res.setHeader('Content-Type', 'text/html');
+
+  const context = {};
+  const { html, error, rootContainer } = await render({
+    // 有需要可带上 query
+    path: req.url,
+    context
+
+    // 可自定义 html 模板
+    // htmlTemplate: defaultHtml,
+
+    // 启用流式渲染
+    // mode: 'stream',
+
+    // html 片段静态标记（适用于静态站点生成）
+    // staticMarkup: false,
+
+    // 扩展 getInitialProps 在服务端渲染中的参数
+    // getInitialPropsCtx: {},
+
+    // manifest，正常情况下不需要
+  });
+
+  // support stream content
+  // if (content instanceof Stream) {
+  //   html.pipe(res);
+  //   html.on('end', function() {
+  //     res.end();
+  //   });
+  // } else {
+  //   res.send(res);
+  // }
+});
+
+app.listen(port, err => {
+  if (err) {
+    throw err;
+  }
+  console.log(`> Ready on port ${port}, ${domain}:${port}`);
+});
